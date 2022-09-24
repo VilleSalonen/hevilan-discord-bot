dada9fe3cdb99492c37b44e039972257c6acc0e1
Ville Salonen <ville.salonen@iki.fi>
2022-09-24 10:29:01 +0300

    Create some interactions

--- src/index.ts
+++ src/index.ts
@@ -13,4 +13,22 @@ client.once('ready', () => {
     console.log('Ready!');
 });
 
+client.on('interactionCreate', async (interaction) => {
+    if (!interaction.isChatInputCommand()) return;
+
+    const { commandName } = interaction;
+
+    if (commandName === 'ping') {
+        await interaction.reply('Pong!');
+    } else if (commandName === 'server') {
+        if (interaction.guild) {
+            await interaction.reply(
+                `Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`
+            );
+        }
+    } else if (commandName === 'user') {
+        await interaction.reply('User info.');
+    }
+});
+
 client.login(token);
